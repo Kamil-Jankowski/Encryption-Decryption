f7095cca072bbda466dc5a2e03af8d7e9ac8bc59
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-09-01 11:13:17 +0200

    Update Main.java

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,27 @@
 package encryptdecrypt;
 
+import java.util.*;
+
 public class Main {
+    private static Locale localePL = new Locale.Builder().setLanguage("pl").setRegion("PL").build();
     public static void main(String[] args) {
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner scanner = new Scanner(System.in);
+        char[] text = scanner.nextLine().toLowerCase(localePL).toCharArray();
+        int key = scanner.nextInt();
+        List<Character> alphabet = Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+        String cyphertext = "";
+
+        for (int i = 0; i < text.length; i++){
+            if (alphabet.contains(text[i])){
+                if (alphabet.indexOf(text[i]) < alphabet.size() - key){
+                    cyphertext += alphabet.get(alphabet.indexOf(text[i]) + key);
+                } else {
+                    cyphertext += alphabet.get(alphabet.indexOf(text[i]) + key - alphabet.size());
+                }
+            } else {
+                cyphertext += text[i];
+            }
+        }
+        System.out.println(cyphertext);
     }
 }
