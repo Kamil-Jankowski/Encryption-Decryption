228e9f7b1abdf3b3e6c7b2b0831324b78e093b29
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-09-08 19:48:39 +0200

    Stage 4 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -3,51 +3,79 @@ package encryptdecrypt;
 import java.util.*;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String method = "enc";						// default method set to encryption
+    private static String data;
+    private static int key;
+    private static String message;
+    private static String output;
     private final static int unicodeMin = 32;
     private final static int unicodeMax = Character.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String method = scanner.nextLine();
-        String text = scanner.nextLine();
-        int key = scanner.nextInt();
-        String output;
+    public static void main(String[] args){
+        // ---------- reading parameters from cmd ----------
+        for (int i = 0, j = 1; j < args.length; i++, j++){
+            if (args[i].equals("-mode") && (!args[j].equals("-data") || !args[j].equals("-key"))){
+                method = args[j];
+            } else if (args[i].equals("-data") && (!args[j].equals("-mode") || !args[j].equals("-key"))){
+                data = args[j];
+            } else if (args[i].equals("-key") && !(args[j].equals("-data") || args[j].equals("-mode"))){
+                key = Integer.parseInt(args[j]);
+            }
+        }
+        // ---------- checking if data provided ----------
+        if (data == null){
+            System.out.println("-data: ");
+            data = scanner.nextLine();
+            System.out.println("-key: ");
+            key = scanner.nextInt();
+        }
+/* Above checker is correct according to provided task, however I think solution below is more usefull:
 
+        // ---------- checking if all parameters are provided ----------
+        if (data == null){
+            System.out.println("-data: ");
+            data = scanner.nextLine();
+        }
+        if (key == 0){
+            System.out.println("-key: ");
+            key = scanner.nextInt();
+        }
+
+ */
+        // ---------- selecting program mode ----------
         if (method.equals("enc")){
-            output = encrypt(text, key);
+            message = encryption(data, key);
         } else if (method.equals("dec")){
-            output = decrypt(text, key);
-        } else {
-            output = "Wrong input. Program will now terminate.";
+            message = decryption(data, key);
         }
-        System.out.println(output);
+        // ---------- printing result of the program ----------
+        System.out.println(message);
     }
 
-    private static String encrypt(String m, int k){
-        char[] text = m.toCharArray();
-        char[] cyphertext = new char[text.length];
-
-        for (int i = 0; i < text.length; i++){
-            if (text[i] < unicodeMax - k){
-                cyphertext[i] = (char) (text[i] + k);
+    private static String encryption(String m, int k){
+        char[] array = m.toCharArray();
+        for (int i = 0; i < array.length; i++){
+            if (array[i]+k <= unicodeMax){
+                array[i] = (char) (array[i]+k);
             } else {
-                cyphertext[i] = (char) (text[i] + k + unicodeMin - unicodeMax);
+                array[i] = (char) (array[i]+k - unicodeMax + unicodeMin);
             }
         }
-        return String.valueOf(cyphertext);
+        output = String.valueOf(array);
+        return output;
     }
 
-    private static String decrypt(String m, int k){
-        char[] text = m.toCharArray();
-        char[] message = new char[text.length];
-
-        for (int i = 0; i < text.length; i++){
-            if (text[i] >= unicodeMin + k){
-                message[i] = (char) (text[i] - k);
+    private static String decryption(String m, int k){
+        char[] array = m.toCharArray();
+        for (int i = 0; i < array.length; i++){
+            if (array[i]-k >= unicodeMin){
+                array[i] = (char) (array[i]-k);
             } else {
-                message[i] = (char) (text[i] - k - unicodeMin + unicodeMax);
+                array[i] = (char) (array[i]-k + unicodeMax - unicodeMin);
             }
         }
-        return String.valueOf(message);
+        output = String.valueOf(array);
+        return output;
     }
 }
