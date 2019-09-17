f8bd253562123c51851770bd4c7ed9b9db50b8ec
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-09-17 19:13:24 +0200

    Stage 5 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,56 +1,68 @@
 package encryptdecrypt;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
     private static String method = "enc";						// default method set to encryption
     private static String data;
     private static int key;
+    private static String inputPath;
+    private static String outputPath;
     private static String message;
     private static String output;
-    private final static int unicodeMin = 32;
+    private final static int unicodeMin = 0;
     private final static int unicodeMax = Character.MAX_VALUE;
 
     public static void main(String[] args){
-        // ---------- reading parameters from cmd ----------
-        for (int i = 0, j = 1; j < args.length; i++, j++){
-            if (args[i].equals("-mode") && (!args[j].equals("-data") || !args[j].equals("-key"))){
-                method = args[j];
-            } else if (args[i].equals("-data") && (!args[j].equals("-mode") || !args[j].equals("-key"))){
-                data = args[j];
-            } else if (args[i].equals("-key") && !(args[j].equals("-data") || args[j].equals("-mode"))){
-                key = Integer.parseInt(args[j]);
+        try {
+            // ---------- reading parameters from cmd ----------
+            for (int i = 0, j = 1; j < args.length; i+=2, j+=2){
+                if (args[i].equals("-mode") && !args[j].startsWith("-")){
+                    method = args[j];
+                } else if (args[i].equals("-data") && !args[j].startsWith("-")){
+                    data = args[j];
+                } else if (args[i].equals("-key") && !args[j].startsWith("-")){
+                    key = Integer.parseInt(args[j]);
+                } else if (args[i].equals("-in") && !args[j].startsWith("-")){
+                    inputPath = args[j];
+                } else if (args[i].equals("-out") && !args[j].startsWith("-")){
+                    outputPath = args[j];
+                } else {										// incorrect parameter name provided
+                    throw new IllegalArgumentException();
+                }
             }
+            // ---------- reading input and checking if data provided ----------
+            if (inputPath != null && data == null){
+                data = readFromFile(inputPath);
+            } else if (inputPath == null && data == null){
+                System.out.println("-data: ");
+                data = scanner.nextLine();
+            }
+            // ---------- selecting program mode ----------
+            if (method.equals("enc")){
+                message = encryption(data, key);
+            } else if (method.equals("dec")){
+                message = decryption(data, key);
+            }
+            // ---------- writing / printing result of the program ----------
+            if (outputPath != null && data != null){
+                writeToFile(outputPath, message);
+            } else {
+                System.out.println(message);
+            }
+        } catch (IllegalArgumentException e){
+            System.out.println("Wrong input provided");
+            System.out.println("Correct application parameters are:");
+            System.out.println("-mode <enc/dec>");
+            System.out.println("-key int");
+            System.out.println("-in <path to file in quotes> OR -data String");
+            System.out.println("-out <path to file in quotes>");
+        } catch (NullPointerException ex){
+            System.out.println("File cannot be created");
         }
-        // ---------- checking if data provided ----------
-        if (data == null){
-            System.out.println("-data: ");
-            data = scanner.nextLine();
-            System.out.println("-key: ");
-            key = scanner.nextInt();
-        }
-/* Above checker is correct according to provided task, however I think solution below is more useful:
-
-        // ---------- checking if all parameters are provided ----------
-        if (data == null){
-            System.out.println("-data: ");
-            data = scanner.nextLine();
-        }
-        if (key == 0){
-            System.out.println("-key: ");
-            key = scanner.nextInt();
-        }
-
- */
-        // ---------- selecting program mode ----------
-        if (method.equals("enc")){
-            message = encryption(data, key);
-        } else if (method.equals("dec")){
-            message = decryption(data, key);
-        }
-        // ---------- printing result of the program ----------
-        System.out.println(message);
     }
 
     private static String encryption(String m, int k){
@@ -77,5 +89,31 @@ public class Main {
         }
         output = String.valueOf(array);
         return output;
+    }
+
+    private static String readFromFile(String path){
+        File file = new File(path);
+        String inputString = "";
+        try(Scanner scanner = new Scanner(file, StandardCharsets.UTF_8)){
+            while (scanner.hasNext()){
+                inputString = inputString.concat(scanner.nextLine()).concat("\n");
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            throw new NullPointerException();
+        }
+        return inputString;
+    }
+
+    private static void writeToFile(String path, String text){
+        try{
+            File file = new File(path);
+            FileWriter writer = new FileWriter(file);
+            writer.write(text);
+            writer.close();
+            System.out.println("File " + path + " saved successfully");
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
